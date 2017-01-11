e366679a2603cb6dea7d5f60409dfc76f614f648
Murph Grainger <murph.grainger@gmail.com>
2017-01-11 15:25:43 -0700

    all authors route works

---
+++ scripts/authors.js
@@ -0,0 +1,34 @@
+const SERVER_URL = getUrl();
+
+$(document).ready(function() {
+    getAuthors()
+        .then(showAuthors);
+});
+
+
+function getAuthors() {
+    return $.get(`${SERVER_URL}/authors/all`);
+}
+
+function showAuthors(data) {
+    let source = $('#card-template').html();
+    let template = Handlebars.compile(source);
+    let context = {
+        data
+    };
+    let html = template(context);
+    $('.cards').html(html);
+    return data;
+}
+
+
+
+
+
+function getUrl() {
+    if (window.location.host.indexOf('localhost') != -1) {
+        return 'http://localhost:3000';
+    } else {
+        return 'https://line-waiter-db.herokuapp.com';
+    }
+}
