b303a2b8b2f37749ebf8a885775df6a2a888496a
Murph Grainger <murph.grainger@gmail.com>
2017-01-12 16:19:14 -0700

    per book view complete

--- scripts/books.js
+++ scripts/books.js
@@ -1,32 +1,24 @@
 const SERVER_URL = getUrl();
 
-
 $(document).ready(function() {
     getBooks()
         .then(showBooks);
 });
-
 
 function getBooks() {
     return $.get(`${SERVER_URL}/books`);
 }
 
 function showBooks(data) {
-    console.log(data);
     let source = $('#card-template').html();
     let template = Handlebars.compile(source);
     let context = {
         data
     };
-    console.log(context);
     let html = template(context);
     $('.cards').html(html);
     return data;
 }
-
-
-
-
 
 function getUrl() {
     if (window.location.host.indexOf('localhost') != -1) {
---
+++ scripts/eachbook.js
@@ -0,0 +1,61 @@
+const SERVER_URL = getUrl();
+const CLIENT_URL = getUrl2();
+const cleanQuery = queryParse(window.location.search);
+
+$(document).ready(function() {
+    getBooks(cleanQuery.id)
+        .then(addBooktoPage)
+        .catch(errorFunction);
+});
+
+function queryParse(query) {
+    let obj = {};
+    let arr1 = query.substr(1).split('&').forEach((element) => {
+        let arr2 = element.split('=');
+        obj[arr2[0]] = arr2[1];
+    });
+    return obj;
+}
+
+function getBooks(id) {
+    return $.get(`${SERVER_URL}/books/${id}`);
+}
+
+function addBooktoPage(book) {
+    let source = $('#card-template').html();
+    let template = Handlebars.compile(source);
+    let context = {
+        book
+    };
+    let html = template(context);
+    $('.cards').html(html);
+    return book;
+}
+
+function getURL() {
+    if (window.location.host.indexOf('localhost') != -1) {
+        return 'http://localhost:3000';
+    } else {
+        return 'https://cruddydegree.herokuapp.com';
+    }
+}
+
+function errorFunction() {
+    alert('An Error Occured');
+}
+
+function getUrl() {
+    if (window.location.host.indexOf('localhost') != -1) {
+        return 'http://localhost:3000';
+    } else {
+        return 'https://line-waiter-db.herokuapp.com';
+    }
+}
+
+function getUrl2() {
+    if (window.location.host.indexOf('localhost') != -1) {
+        return 'http://localhost:8080';
+    } else {
+        return 'https://line-waiter.firebaseapp.com';
+    }
+}
