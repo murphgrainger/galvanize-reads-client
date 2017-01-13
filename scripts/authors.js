e5206774e0eda009283a7b4f4d6989d15d3f7c27
Murph Grainger <murph.grainger@gmail.com>
2017-01-12 23:09:18 -0700

    removed duplicate books per author

--- scripts/authors.js
+++ scripts/authors.js
@@ -2,6 +2,7 @@ const SERVER_URL = getUrl();
 
 $(document).ready(function() {
     getAuthors()
+        .then(cleanData)
         .then(showAuthors);
 });
 
@@ -27,4 +28,37 @@ function getUrl() {
     } else {
         return 'https://galvanize-reads-mg.herokuapp.com';
     }
+}
+
+function cleanData(data) {
+    let bookIndex = {};
+    let authorList = [];
+    data.forEach(author => {
+        console.log(author);
+        var newAuthor = {};
+        var book = {};
+        newAuthor.id = author.author_id;
+        newAuthor.fname = author.fname;
+        newAuthor.lname = author.lname;
+        newAuthor.biography = author.biography;
+        newAuthor.portrait = author.portrait;
+        book.id = author.book_id;
+        book.title = author.title;
+        book.description = author.description;
+        book.cover = author.cover;
+        authorList.push(newAuthor);
+        bookIndex[author.author_id] = bookIndex[author.author_id] || [];
+        bookIndex[author.author_id].push(book);
+    });
+    return authorList.reduce((authors, author, index, array) => {
+        if (array[index + 1] && author.id !== array[index + 1].id) {
+            author.books = bookIndex[author.id];
+            authors.push(author);
+        } else if (!array[index + 1]) {
+            author.books = bookIndex[author.id];
+            authors.push(author);
+        }
+        console.log(authors);
+        return authors;
+    }, []);
 }
